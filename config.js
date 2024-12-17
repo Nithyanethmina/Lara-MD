da0e7afd345c6bef5a3c6938aa15853b307e14dc
Nithyanethmina <135350015+Nithyanethmina@users.noreply.github.com>
2024-12-17 19:28:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,13 +6,13 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "zigAxYJQ#eft1fsttXAjKdh5qbTXextKHyj3iHyMtlt_1bvsaOZE", //paste your session_id
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false", 
 MODE: process.env.MODE || "public",
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 AUTO_REACT: process.env.AUTO_REACT || "true",
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true",    
+FAKE_RECORDING: process.env.FAKE_RECORDING || "false",    
 ANTI_LINK: process.env.ANTI_LINK || "false",
 ANTI_BAD: process.env.ANTI_BAD || "true",    
 };
